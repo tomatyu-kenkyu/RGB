ac4bde0e4a4313ec3dd5df4f3939bdef82f024e2
tomatyu-kenkyu <tomatyu2008@gmail.com>
2026-04-16 06:33:50 +0000

    ｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,5 @@
-import streamlit as st
+import pytesseract
 from PIL import Image
-import numpy as np
-from paddleocr import PaddleOCR
 
-st.title("OCRアプリ")
-
-@st.cache_resource
-def load_ocr():
-    return PaddleOCR(use_angle_cls=True, lang='japanese')
-
-ocr = load_ocr()
-
-uploaded_file = st.file_uploader("画像アップロード", type=["png","jpg","jpeg"])
-
-if uploaded_file:
-    image = Image.open(uploaded_file)
-    img_np = np.array(image)
-
-    result = ocr.ocr(img_np)
-
-    for line in result[0]:
-        st.write(line[1][0])
+text = pytesseract.image_to_string(Image.open("test.png"), lang="jpn")
+print(text)
