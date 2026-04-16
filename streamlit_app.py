5866bb17b5638b08e0abc9f160ba1b44de873dcd
tomatyu-kenkyu <tomatyu2008@gmail.com>
2026-04-16 06:34:54 +0000

    え

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
-import pytesseract
+import streamlit as st
 from PIL import Image
+import pytesseract
 
-text = pytesseract.image_to_string(Image.open("test.png"), lang="jpn")
-print(text)
+st.title("OCRアプリ（Tesseract）")
+
+uploaded_file = st.file_uploader("画像をアップロード", type=["png","jpg","jpeg"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    
+    text = pytesseract.image_to_string(image, lang="jpn")
+    
+    st.image(image, caption="アップロード画像")
+    st.write("抽出結果👇")
+    st.write(text)
